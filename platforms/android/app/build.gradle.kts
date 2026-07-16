0def97c9bf4952d6b804a616e0103fee5a551409
kena77104-design <kena77104@gmail.com>
2026-07-16 03:45:33 +0300

    Update build.gradle.kts

--- platforms/android/app/build.gradle.kts
+++ platforms/android/app/build.gradle.kts
@@ -199,6 +199,8 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.composeIcons.fontAwesome)
     implementation(libs.composeIcons.lineAwesome)
 
